4a8d5727cd57b48f4c653770313680db01dd23d4
Xiaohui WANG <xwang227@ds2.CS.McGill.CA>
2017-12-04 16:40:36 -0500

    add EOF to DATA.txt when mydata.txt reaches the end

---
+++ producer.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int producer(){
+	FILE *t_fpt;
+	FILE *d_fpt;
+	FILE *m_fpt;
+	char turn='0';
+	char read=' ';
+	int pos=0;
+	//const pos=0;
+
+
+	m_fpt=fopen("mydata.txt","r");	//first open the file to read
+	if(m_fpt==NULL) exit(1);
+
+//	read = fgetc(m_fpt);
+
+	while(read!=EOF){ 
+
+		while((t_fpt=fopen("TURN.txt","r+"))==NULL);
+		
+
+			turn=fgetc(t_fpt);
+//	printf("opened TURN.txt: %c",turn);
+			fclose(t_fpt);
+
+			if (turn<'0'&&turn>'1'){
+				printf("producer in exit turn is:%c",turn);
+			exit(1);
+			}
+
+			if(turn=='0'){
+				printf("producer turn\n");
+				// while((t_fpt=fopen("TURN.txt","w+"))==NULL);
+
+				//write to data before write to turn
+				while((d_fpt=fopen("DATA.txt","r+"))==NULL);
+				read = fgetc(m_fpt);		//get the char from mydata.txt
+		//adding condition
+				if(read!=EOF){
+					fputc(read,d_fpt);
+				}		
+//				fclose(d_fpt);	//close data.txt
+
+				if(read==EOF){
+					fputc('\0',d_fpt);
+				}		
+				fclose(d_fpt);
+		
+				while((t_fpt=fopen("TURN.txt","w+"))==NULL);
+				//rewind(t_fpt);
+				fputc('1',t_fpt);
+				printf("now turn is set to 1 consumer\n");
+				fclose(t_fpt);
+
+	// 			while((d_fpt=fopen("DATA.txt","w"))==NULL);
+	// 			read = fgetc(m_fpt);		//get the char from mydata.txt
+	// //adding condition
+	// 			if(read!=EOF){
+	// 				fputc(read,d_fpt);
+	// 			}			
+
+	//fputc(read,d_fpt);
+				printf("input in data.txt char is: %c\n",read);
+				// fclose(d_fpt);
+		//	fclose(t_fpt);
+
+				printf("------------\n");
+		//	fseek(m_fpt,1,SEEK_CUR);
+			}
+	}
+//fclose(m_fpt);
+
+//read is updated through fgetc
+//terminate the program
+// if(read==EOF){
+	exit(0); 
+// }
+
+	fclose(m_fpt);
+	return 0;
+}
+
